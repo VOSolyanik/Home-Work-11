d4ec4c5e1f10abb6a6cc13c23b4406b0ee6cdeeb
VOSolyanik <v.o.solyanik@gmail.com>
2014-06-24 15:24:39 +0300

    everithin finished

--- Bluemist/js/script.js
+++ Bluemist/js/script.js
@@ -1,3 +1,15 @@
 $(function () {
-	
+	var $tabButtons = $('.tab-buttons li a'),
+		$tabsContent = $('.tabs li');
+
+	$tabsContent.removeClass('no-script');	
+
+	$tabButtons.on('click', function(event) {
+		event.preventDefault();
+		$tabButtons.removeClass('active-tab-button');
+		$(this).addClass('active-tab-button');
+
+		$tabsContent.removeClass('active-tab');
+		$($(this).attr('href')).addClass('active-tab');		
+	});
 });
