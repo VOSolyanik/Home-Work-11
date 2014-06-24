4bf19212c51072f07e1d7d89bb7e67e8597d0b1d
VOSolyanik <v.o.solyanik@gmail.com>
2014-06-24 15:24:39 +0300

    everything finished

--- Bluemist/config.rb
+++ Bluemist/config.rb
@@ -13,8 +13,7 @@ javascripts_dir = "js"
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
-# To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+# To disable debugging comments that display the original location of your selectors. Uncomment:# line_comments = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
